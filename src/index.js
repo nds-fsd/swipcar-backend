26a3294e0ef5080a640c65bd86de36d3075d5d7b
Xavier <joubertxavier@gmail.com>
2021-03-25 10:37:06 +0100

    Set-up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const app = express();
 const cors = require('cors');
 app.use(cors())
+const bodyParser = require("body-parser");
 const server = app.listen(process.env.PORT, () => {
     console.log('server is running on port', server.address().port);
 });
