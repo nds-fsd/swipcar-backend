67c65325b4dc3578e5e798e94401be0624ba48d0
Xavier <joubertxavier@gmail.com>
2021-04-08 10:48:58 +0200

    update Routers & Schemas

--- src/mongo/schemas/carType.js
+++ src/mongo/schemas/carType.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema(
   {
-    brandname: { type: String, required: true, unique: true },
-    logotype: { type: String, required: true },
+    urbanita: { type: Boolean, required: true },
+    suv: { type: Boolean, required: true },
+    suvsmall: { type: Boolean, required: true },
+    compacto: { type: Boolean, required: true },
+    berlina: { type: Boolean, required: true },
+    _7plazas: { type: Boolean, required: true },
+    _4x4: { type: Boolean, required: true },
   },
   { timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } }
 );
